5cdc2bfa0c16f7ba49addb44cefdb5013c2cd9ff
btn <0xmb@protonmail.com>
2022-03-30 20:00:13 +0200

    add paragons func

--- months.py
+++ months.py
@@ -12,74 +12,84 @@ def ford_completing():
         ws1['E8'] = int(y)
         z = input(f"{sheet.title.capitalize()} fuel start: ")
         ws1['E10'] = int(z)
-        paragons.paragons()
+        paragons.paragons_ford()
     elif choose == '2':
         ws2 = start.wb['luty']
         sheet = start.wb.get_sheet_by_name('luty')
         print(f"You choose {sheet.title}")
         x = input(f"{sheet.title.capitalize()} odometer end: ")
         ws2['E8'] = int(x)
-        paragons.paragons()
+        paragons.paragons_ford()
     elif choose == '3':
         ws3 = start.wb['marzec']
         sheet = start.wb.get_sheet_by_name('marzec')
         print(f"You choose {sheet.title}")
         x = input(f"{sheet.title.capitalize()} odometer end: ")
         ws3['E8'] = int(x)
+        paragons.paragons_ford()
     elif choose == '4':
         ws4 = start.wb['kwiecien']
         sheet = start.wb.get_sheet_by_name('kwiecien')
         print(f"You choose {sheet.title}")
         x = input(f"{sheet.title.capitalize()} odometer end: ")
         ws4['E8'] = int(x)
+        paragons.paragons_ford()
     elif choose == '5':
         ws5 = start.wb['maj']
         sheet = start.wb.get_sheet_by_name('maj')
         print(f"You choose {sheet.title}")
         x = input(f"{sheet.title.capitalize()} odometer end: ")
         ws5['E8'] = int(x)
+        paragons.paragons_ford()
     elif choose == '6':
         ws6 = start.wb['czerwiec']
         sheet = start.wb.get_sheet_by_name('czerwiec')
         print(f"You choose {sheet.title}")
         x = input(f"{sheet.title.capitalize()} odometer end: ")
         ws6['E8'] = int(x)
+        paragons.paragons_ford()
     elif choose == '7':
         ws7 = start.wb['lipiec']
         sheet = start.wb.get_sheet_by_name('lipiec')
         print(f"You choose {sheet.title}")
         x = input(f"{sheet.title.capitalize()} odometer end: ")
         ws7['E8'] = int(x)
+        paragons.paragons_ford()
     elif choose == '8':
         ws8 = start.wb['sierpien']
         sheet = start.wb.get_sheet_by_name('sierpien')
         print(f"You choose {sheet.title}")
         x = input(f"{sheet.title.capitalize()} odometer end: ")
         ws8['E8'] = int(x)
+        paragons.paragons_ford()
     elif choose == '9':
         ws9 = start.wb['wrzesien']
         sheet = start.wb.get_sheet_by_name('wrzesien')
         print(f"You choose {sheet.title}")
         x = input(f"{sheet.title.capitalize()} odometer end: ")
         ws9['E8'] = int(x)
+        paragons.paragons_ford()
     elif choose == '10':
         ws10 = start.wb['pazdziernik']
         sheet = start.wb.get_sheet_by_name('pazdziernik')
         print(f"You choose {sheet.title}")
         x = input(f"{sheet.title.capitalize()} odometer end: ")
         ws10['E8'] = int(x)
+        paragons.paragons_ford()
     elif choose == '11':
         ws11 = start.wb['listopad']
         sheet = start.wb.get_sheet_by_name('listopad')
         print(f"You choose {sheet.title}")
         x = input(f"{sheet.title.capitalize()} odometer end: ")
         ws11['E8'] = int(x)
+        paragons.paragons_ford()
     else:
         ws12 = start.wb['grudzien']
         sheet = start.wb.get_sheet_by_name('grudzien')
         print(f"You choose {sheet.title}")
         x = input(f"{sheet.title.capitalize()} odometer end: ")
         ws12['E8'] = int(x)
+        paragons.paragons_ford()
 
     start.wb.save("1.FORD COURIER.xlsx")
 
@@ -96,6 +106,7 @@ def skoda_completing():
         ws1['E8'] = int(y)
         z = input(f"{sheet.title.capitalize()} fuel start: ")
         ws1['E10'] = int(z)
+        paragons.paragons_skoda()
     elif choose == '2':
         # mth = start.ws['C5']
         ws2 = start.wb1['luty']
@@ -103,66 +114,77 @@ def skoda_completing():
         print(f"You choose {sheet.title}")
         x = input(f"{sheet.title.capitalize()} odometer end: ")
         ws2['E8'] = int(x)
+        paragons.paragons_skoda()
     elif choose == '3':
         ws3 = start.wb1['marzec']
         sheet = start.wb1.get_sheet_by_name('marzec')
         print(f"You choose {sheet.title}")
         x = input(f"{sheet.title.capitalize()} odometer end: ")
         ws3['E8'] = int(x)
+        paragons.paragons_skoda()
     elif choose == '4':
         ws4 = start.wb1['kwiecien']
         sheet = start.wb1.get_sheet_by_name('kwiecien')
         print(f"You choose {sheet.title}")
         x = input(f"{sheet.title.capitalize()} odometer end: ")
         ws4['E8'] = int(x)
+        paragons.paragons_skoda()
     elif choose == '5':
         ws5 = start.wb1['maj']
         sheet = start.wb1.get_sheet_by_name('maj')
         print(f"You choose {sheet.title}")
         x = input(f"{sheet.title.capitalize()} odometer end: ")
         ws5['E8'] = int(x)
+        paragons.paragons_skoda()
     elif choose == '6':
         ws6 = start.wb1['czerwiec']
         sheet = start.wb1.get_sheet_by_name('czerwiec')
         print(f"You choose {sheet.title}")
         x = input(f"{sheet.title.capitalize()} odometer end: ")
         ws6['E8'] = int(x)
+        paragons.paragons_skoda()
     elif choose == '7':
         ws7 = start.wb1['lipiec']
         sheet = start.wb1.get_sheet_by_name('lipiec')
         print(f"You choose {sheet.title}")
         x = input(f"{sheet.title.capitalize()} odometer end: ")
         ws7['E8'] = int(x)
+        paragons.paragons_skoda()
     elif choose == '8':
         ws8 = start.wb1['sierpien']
         sheet = start.wb1.get_sheet_by_name('sierpien')
         print(f"You choose {sheet.title}")
         x = input(f"{sheet.title.capitalize()} odometer end: ")
         ws8['E8'] = int(x)
+        paragons.paragons_skoda()
     elif choose == '9':
         ws9 = start.wb1['wrzesien']
         sheet = start.wb1.get_sheet_by_name('wrzesien')
         print(f"You choose {sheet.title}")
         x = input(f"{sheet.title.capitalize()} odometer end: ")
         ws9['E8'] = int(x)
+        paragons.paragons_skoda()
     elif choose == '10':
         ws10 = start.wb1['pazdziernik']
         sheet = start.wb1.get_sheet_by_name('pazdziernik')
         print(f"You choose {sheet.title}")
         x = input(f"{sheet.title.capitalize()} odometer end: ")
         ws10['E8'] = int(x)
+        paragons.paragons_skoda()
     elif choose == '11':
         ws11 = start.wb1['listopad']
         sheet = start.wb1.get_sheet_by_name('listopad')
         print(f"You choose {sheet.title}")
         x = input(f"{sheet.title.capitalize()} odometer end: ")
         ws11['E8'] = int(x)
+        paragons.paragons_skoda()
     else:
         ws12 = start.wb1['grudzien']
         sheet = start.wb1.get_sheet_by_name('grudzien')
         print(f"You choose {sheet.title}")
         x = input(f"{sheet.title.capitalize()} odometer end: ")
         ws12['E8'] = int(x)
+        paragons.paragons_skoda()
 
     start.wb1.save("2.SKODA FABIA.xlsx")
 
@@ -180,7 +202,6 @@ def skoda_2_completing():
         z = input(f"{sheet.title.capitalize()} fuel start: ")
         ws1['E10'] = int(z)
     elif choose == '2':
-        # mth = start.ws['C5']
         ws2 = start.wb2['luty']
         sheet = start.wb2.get_sheet_by_name('luty')
         print(f"You choose {sheet.title}")
