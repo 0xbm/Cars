159e0c2c04b7f0bd3ad477753da98f557ee2bc01
0xbm <0xmb@protonmail.com>
2022-11-03 18:25:49 +0100

    foramtted code and add input path

--- cars.py
+++ cars.py
@@ -8,7 +8,9 @@ import re
 class DisplayMenu:
     def sort_years(self):
         path = os.getcwd()
-        folders_sort = ([name for name in os.listdir(path) if os.path.isdir(os.path.join(path, name))])
+        folders_sort = [
+            name for name in os.listdir(path) if os.path.isdir(os.path.join(path, name))
+        ]
         folders_sort.reverse()
         folders_exception = re.compile(r".*(venv|.idea)")
         print([folders_exception.sub("", x) for x in folders_sort])
@@ -16,67 +18,69 @@ class DisplayMenu:
     def choose_year(self):
         choose = input("Choose year: 1.2022, 2.2023: ")
         if choose == "1":
-            os.chdir("/Users/btn/PycharmProjects/cars/2022")
+            os.chdir(path)
             print("You choosed year: 2022")
             t.choose_cars()
         if choose == "2":
-            os.chdir("/Users/btn/PycharmProjects/cars/2023")
+            os.chdir(path)
             print("You choosed year: 2023")
             t.choose_cars()
 
     def choose_cars(self):
-        path = (os.getcwd())
+        path = os.getcwd()
         files = [f for f in glob.glob(path + "**/*.xlsx", recursive=True)]
         folders_show = re.compile(r".*(/2022/|/2023/)")
-        files_sort = natsorted([folders_show.sub('', x).strip() for x in files])
+        files_sort = natsorted([folders_show.sub("", x).strip() for x in files])
         print(files_sort)
         choose = input("Select vehicle number: ")
-        if choose == '1':
+        if choose == "1":
             print("You selected: ", files_sort[0])
             months.ford_completing()
-        if choose == '2':
+        if choose == "2":
             print("You selected: ", files_sort[1])
             months.skoda_completing()
-        if choose == '3':
+        if choose == "3":
             print("You selected: ", files_sort[2])
             months.skoda_2_completing()
-        if choose == '4':
+        if choose == "4":
             print("You selected: ", files_sort[3])
             months.fiat_completing()
-        if choose == '5':
+        if choose == "5":
             print("You selected: ", files_sort[4])
             months.citroen_completing()
-        if choose == '6':
+        if choose == "6":
             print("You selected: ", files_sort[5])
             months.daewoo_completing()
-        if choose == '7':
+        if choose == "7":
             print("You selected: ", files_sort[6])
             months.ford_2_completing()
-        if choose == '8':
+        if choose == "8":
             print("You selected: ", files_sort[7])
             months.ford_leasing_completing()
-        if choose == '9':
+        if choose == "9":
             print("You selected: ", files_sort[8])
             months.farmtrac_completing()
-        if choose == '10':
+        if choose == "10":
             print("You selected: ", files_sort[9])
             months.ursus_completing()
-        if choose == '11':
+        if choose == "11":
             print("You selected: ", files_sort[10])
             months.tym_completing()
-        if choose == '12':
+        if choose == "12":
             print("You selected: ", files_sort[11])
             months.unimog_completing()
-        if choose == '13':
+        if choose == "13":
             print("You selected: ", files_sort[12])
             months.unimog_2_completing()
-        if choose == '14':
+        if choose == "14":
             print("You selected: ", files_sort[13])
             months.noremat_completing()
 
 
+path = input("Path to year: ")
+
 while True:
-    message = input("Enter to continue, q to quit")
+    message = input("Enter to continue, q to quit: ")
     if message == "q":
         break
     else:
